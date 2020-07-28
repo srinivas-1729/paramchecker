e7c00b3066be23e7434fe977ac35f6b1e3da8e80
srinivas-1729 <68850958+srinivas-1729@users.noreply.github.com>
2020-07-28 15:47:57 +0530

    Update new test case for readings ok function

--- testparamchecker.cpp
+++ testparamchecker.cpp
@@ -8,7 +8,11 @@ TEST(VitalsTest, BPM) {
 TEST(VitalsTest, SPO2) {
     ASSERT_EQ(false, vitalsAreOk(100, 40, 50));
 }
- 
+
+TEST(VitalsTest, ReadingsOk) {
+    ASSERT_EQ(true, isReadingsOk(30, 10, 70));
+}
+
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
