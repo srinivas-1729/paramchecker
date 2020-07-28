c8f56b235d170a6748c20d5dc7435b9fdf51e6c9
srinivas-1729 <68850958+srinivas-1729@users.noreply.github.com>
2020-07-28 15:50:44 +0530

    Move prototype functions to header file

--- paramchecker.h
+++ paramchecker.h
@@ -1,2 +1,7 @@
 
+bool bpmIsOk(float bpm);
+bool spo2IsOk(float spo2);
+bool respRateIsOk(float respRate);
+bool isReadingsOk(float val, float min_lim, float max_lim);
+
 bool vitalsAreOk(float bpm, float spo2, float respRate);
