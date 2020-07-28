b051dacd5b24033700f49d1024b91997396ca2d0
srinivas-1729 <68850958+srinivas-1729@users.noreply.github.com>
2020-07-28 15:50:09 +0530

    Move protos to header file

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -1,9 +1,3 @@
-
-bool bpmIsOk(float bpm);
-bool spo2IsOk(float spo2);
-bool respRateIsOk(float respRate);
-bool isReadingsOk(float val, float min_lim, float max_lim);
-
 bool isReadingsOk(float val, float min_lim, float max_lim)
 {
  bool retVal = true;
