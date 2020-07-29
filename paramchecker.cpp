5633a8668c8fd80ab16faaa000d2474b03790678
Srinivas H <hsrinu29@gmail.com>
2020-07-29 16:47:49 +0530

    Change the structure configuration

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -1,21 +1,21 @@
 #include "paramchecker.h"
 
-struct vital_config_s {
+typedef struct {
  float vitalVal;
  float vitalMinVal;
  float vitalMaxVal;
-};
+}vital_config_s;
 
-struct vital_param_s {
+typedef struct {
   vital_config_s *vitalValList;
   int numSensors;
-}
+}vital_param_s;
 
 #define BPM_VAL 100
 #define SPO2_VAL 100
 #define RESPRATE_VAL 50 
 
-vital_config_s vitalValList = {
+vital_config_s vitalValList[] = {
  {BPM_VAL, 70, 150},
  {SPO2_VAL, 80, 150},
  {RESPRATE_VAL, 30, 60}
@@ -24,19 +24,18 @@ vital_config_s vitalValList = {
 vital_param_s vitalList = {
   vitalValList,
   sizeof(vitalValList)/sizeof(vitalValList[0])
-}
+};
 
-bool isReadingsOk(float val, float min_lim, float max_lim)
-{
- bool retVal = true;
- if((val < min_lim) || (val > max_lim))
- {
-   retVal = false;
- }
+bool isReadingsOk(float val, float min_lim, float max_lim) {
+  bool retVal = true;
+  if((val < min_lim) || (val > max_lim))
+  {
+    retVal = false;
+  }
   return retVal;
 }
 
-bool vitalsAreOk(float *vitalList) {
+bool vitalsAreOk(vital_param_s *vitalList) {
   
   bool retVal = true;
 
@@ -46,3 +45,5 @@ bool vitalsAreOk(float *vitalList) {
   }
    return retVal;
 }
+
+vitalsAreOk(&vitalList);
